2a1659605c5b1de0d3c5d6f1e7b88b07964797c8
Oli Lane <oli@gearset.com>
2020-02-25 11:09:09 +0000

    Extract spotify.js

--- index.js
+++ index.js
@@ -5,21 +5,8 @@ const secrets = require('./secrets.js');
 const { WebClient } = require('@slack/web-api');
 const _ = require('lodash');
 const spotifyTrackIdExtractor = require('./spotifyTrackIdExtractor.js');
-const SpotifyWebApi = require('spotify-web-api-node');
+const spotify = require('./spotify');
 
-async function delay(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
-
-async function getSpotifyClient() {
-    const spotifyApi = new SpotifyWebApi(secrets.spotifyClientCredentials);
-    spotifyApi.setRefreshToken(secrets.spotifyRefreshToken);
-    
-    const data = await spotifyApi.refreshAccessToken();
-    spotifyApi.setAccessToken(data.body['access_token']);
-
-    return spotifyApi;
-}
 
 function getSlackClient() {
     const slackClient = new WebClient(secrets.slackBotOauthAccessToken);
@@ -27,7 +14,7 @@ function getSlackClient() {
     return slackClient;
 }
 
-async function getAllSpotifyTracksFromSlack(slackClient, channelId) {
+async function getAllSpotifyTracksFromSlack(slackClient, channelId, pageLimit = -1) {
     let tracks = [];
 
     let pageCount = 0;
@@ -43,72 +30,24 @@ async function getAllSpotifyTracksFromSlack(slackClient, channelId) {
         pageCount++;
 
         console.log("Fetched " + theseTracks.length + " tracks from slack");
+
+        if (pageLimit !== -1 && pageCount >= pageLimit) {
+            break;
+        }
     }
 
     return _.uniqBy(tracks, x => x.trackId);
-}
-
-async function getSpotifyTracksInPlaylist(spotifyApi, playlistId) {
-    const limit = 100;
-    let offset = 0;
-    let playlistTracks = [];
-    let total = 0;
-
-    do { 
-        const playlistTracksResponse = await spotifyApi.getPlaylistTracks(playlistId, {
-            limit: limit,
-            offset: offset
-        });
-
-        offset = playlistTracksResponse.body.offset + playlistTracksResponse.body.limit;
-        total = playlistTracksResponse.body.total;
-
-        console.log("Fetched " + playlistTracksResponse.body.limit + " tracks from playlist");
-
-        playlistTracks.push(...playlistTracksResponse.body.items);
-
-        await delay(200);
-    }
-    while (playlistTracks.length < total);
-
-    return playlistTracks;
-}
-
-async function putSpotifyTracksIntoPlaylist(spotifyApi, tracksToAdd, playlistId) {
-    const currentTracks = await getSpotifyTracksInPlaylist(spotifyApi, playlistId);
-
-    const currentTrackIds = currentTracks.map(x => x.track.id);
-    // spotify has some tracks that link from multiple IDs - this also excludes the aliases
-    const currentLinkedFromTrackIds = currentTracks.map(x => x.track.linked_from && x.track.linked_from.id).filter(x => x != null);
-
-    const trackIdsBeingAdded = tracksToAdd.map(x => x.trackId);
-
-    const newTracksToAdd = _.difference(_.difference(trackIdsBeingAdded, currentTrackIds), currentLinkedFromTrackIds);
-
-    if (newTracksToAdd.length === 0) {
-        return 0;
-    }
-
-    const chunked = _.chunk(newTracksToAdd, 20);
-
-    for (const chunk of chunked) {
-        await spotifyApi.addTracksToPlaylist(playlistId, chunk.map(x => 'spotify:track:' + x));
-        console.log("Added " + chunk.length + " tracks");
-        await delay(1000);
-    }
-
-    return newTracksToAdd.length;
 }
 
 (async () => {
 
     try {
         const slackApi = getSlackClient();
-        const tracks = await getAllSpotifyTracksFromSlack(slackApi, secrets.spotifyChannelName);
+        const tracks = await getAllSpotifyTracksFromSlack(slackApi, secrets.spotifyChannelName, 1);
         console.log("Got " + tracks.length + " tracks from slack, total");
 
-        const spotifyApi = await getSpotifyClient();
-        const tracksAdded = await putSpotifyTracksIntoPlaylist(spotifyApi, tracks, '4VgNNTXhy73ZCvqT2MthV5');
+        const spotifyApi = await spotify.getSpotifyClient(secrets);
+        const tracksAdded = await spotify.putSpotifyTracksIntoPlaylist(spotifyApi, tracks, '4VgNNTXhy73ZCvqT2MthV5');
 
         console.log('Success! Added ' + tracksAdded + ' tracks');
 
---
+++ spotify.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const _ = require('lodash');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+async function delay(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function getSpotifyTracksInPlaylist(spotifyApi, playlistId) {
+    const limit = 100;
+    let offset = 0;
+    let playlistTracks = [];
+    let total = 0;
+
+    do { 
+        const playlistTracksResponse = await spotifyApi.getPlaylistTracks(playlistId, {
+            limit: limit,
+            offset: offset
+        });
+
+        offset = playlistTracksResponse.body.offset + playlistTracksResponse.body.limit;
+        total = playlistTracksResponse.body.total;
+
+        console.log("Fetched " + playlistTracksResponse.body.limit + " tracks from playlist");
+
+        playlistTracks.push(...playlistTracksResponse.body.items);
+
+        await delay(200);
+    }
+    while (playlistTracks.length < total);
+
+    return playlistTracks;
+}
+
+async function putSpotifyTracksIntoPlaylist(spotifyApi, tracksToAdd, playlistId) {
+    const currentTracks = await getSpotifyTracksInPlaylist(spotifyApi, playlistId);
+
+    const currentTrackIds = currentTracks.map(x => x.track.id);
+    // spotify has some tracks that link from multiple IDs - this also excludes the aliases
+    const currentLinkedFromTrackIds = currentTracks.map(x => x.track.linked_from && x.track.linked_from.id).filter(x => x != null);
+
+    const trackIdsBeingAdded = tracksToAdd.map(x => x.trackId);
+
+    const newTracksToAdd = _.difference(_.difference(trackIdsBeingAdded, currentTrackIds), currentLinkedFromTrackIds);
+
+    if (newTracksToAdd.length === 0) {
+        return 0;
+    }
+
+    const chunked = _.chunk(newTracksToAdd, 20);
+
+    for (const chunk of chunked) {
+        await spotifyApi.addTracksToPlaylist(playlistId, chunk.map(x => 'spotify:track:' + x));
+        console.log("Added " + chunk.length + " tracks");
+        await delay(1000);
+    }
+
+    return newTracksToAdd.length;
+}
+
+async function getSpotifyClient(secrets) {
+    const spotifyApi = new SpotifyWebApi(secrets.spotifyClientCredentials);
+    spotifyApi.setRefreshToken(secrets.spotifyRefreshToken);
+    
+    const data = await spotifyApi.refreshAccessToken();
+    spotifyApi.setAccessToken(data.body['access_token']);
+
+    return spotifyApi;
+}
+
+module.exports = {
+    getSpotifyClient,
+    putSpotifyTracksIntoPlaylist
+};
