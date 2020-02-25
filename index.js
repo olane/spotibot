836b847ec3b220070a9944c0c40af3b09f4548cb
Oli Lane <oli@gearset.com>
2020-02-25 11:11:25 +0000

    Extract slack.js

--- index.js
+++ index.js
@@ -1,59 +1,19 @@
 'use strict';
 
-const secrets = require('./secrets.js');
-
-const { WebClient } = require('@slack/web-api');
-const _ = require('lodash');
-const spotifyTrackIdExtractor = require('./spotifyTrackIdExtractor.js');
+const secrets = require('./secrets');
 const spotify = require('./spotify');
-
-
-function getSlackClient() {
-    const slackClient = new WebClient(secrets.slackBotOauthAccessToken);
-
-    return slackClient;
-}
-
-async function getAllSpotifyTracksFromSlack(slackClient, channelId, pageLimit = -1) {
-    let tracks = [];
-
-    let pageCount = 0;
-
-    for await (const page of slackClient.paginate('conversations.history', { channel: channelId })) {
-        const theseTracks = _.flatten(page.messages.map(message =>
-            spotifyTrackIdExtractor.extractTrackIds(message.text).map(trackId => ({
-                trackId: trackId,
-                timestamp: message.ts
-            }))
-        ));
-        tracks.push(...theseTracks);
-        pageCount++;
-
-        console.log("Fetched " + theseTracks.length + " tracks from slack");
-
-        if (pageLimit !== -1 && pageCount >= pageLimit) {
-            break;
-        }
-    }
-
-    return _.uniqBy(tracks, x => x.trackId);
-}
+const slack = require('./slack');
 
 (async () => {
-
     try {
-        const slackApi = getSlackClient();
-        const tracks = await getAllSpotifyTracksFromSlack(slackApi, secrets.spotifyChannelName, 1);
+        const slackApi = slack.getSlackClient(secrets);
+        const tracks = await slack.getAllSpotifyTracksFromSlack(slackApi, secrets.spotifyChannelName, 1);
         console.log("Got " + tracks.length + " tracks from slack, total");
 
         const spotifyApi = await spotify.getSpotifyClient(secrets);
         const tracksAdded = await spotify.putSpotifyTracksIntoPlaylist(spotifyApi, tracks, '4VgNNTXhy73ZCvqT2MthV5');
-
         console.log('Success! Added ' + tracksAdded + ' tracks');
-
     } catch (error) {
         console.log(error);
     }
-
 })();
-
---
+++ slack.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { WebClient } = require('@slack/web-api');
+const _ = require('lodash');
+const spotifyTrackIdExtractor = require('./spotifyTrackIdExtractor');
+
+function getSlackClient(secrets) {
+    const slackClient = new WebClient(secrets.slackBotOauthAccessToken);
+
+    return slackClient;
+}
+
+async function getAllSpotifyTracksFromSlack(slackClient, channelId, pageLimit = -1) {
+    let tracks = [];
+
+    let pageCount = 0;
+
+    for await (const page of slackClient.paginate('conversations.history', { channel: channelId })) {
+        const theseTracks = _.flatten(page.messages.map(message =>
+            spotifyTrackIdExtractor.extractTrackIds(message.text).map(trackId => ({
+                trackId: trackId,
+                timestamp: message.ts
+            }))
+        ));
+        tracks.push(...theseTracks);
+        pageCount++;
+
+        console.log("Fetched " + theseTracks.length + " tracks from slack");
+
+        if (pageLimit !== -1 && pageCount >= pageLimit) {
+            break;
+        }
+    }
+
+    return _.uniqBy(tracks, x => x.trackId);
+}
+
+module.exports = {
+    getSlackClient,
+    getAllSpotifyTracksFromSlack
+};
